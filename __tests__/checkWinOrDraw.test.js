39464a4dbcb5e6d306aca1b2ec48142207b7b727
Emad <emadul.islam@hotmail.co.uk>
2023-05-04 17:13:46 +0100

    tested for win conditions in a row, column or diagonal

--- __tests__/checkWinOrDraw.test.js
+++ __tests__/checkWinOrDraw.test.js
@@ -1,26 +1,153 @@
 //rows
-    // top row
-    //all rows
+// top row
+//all rows
 
 //columns
-    // 
+// first column
+// all columns
 //diagonal
-//draw 
- const {checkWinOrDraw} = require("../src/utils/utils")
+//top left to bottom right
+//top right to bottom left
+
+//draw
+
+const { checkWinOrDraw } = require("../src/utils/utils");
 
 describe("checkWinOrDraw", () => {
+  describe("status: game not finished", () => {
     test("returns undefined for empty grid", () => {
-        const grid = [0, 0, 0, 0, 0, 0, 0, 0, 0 ] 
-        expect(checkWinOrDraw(grid)).toBe(undefined)
+      const grid = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+      expect(checkWinOrDraw(grid)).toBe(undefined);
+    });
+    test("returns undefined for non win or non draw grid", () => {
+      const grid = [0, 0, 1, 0, 0, 0, 0, 0, 0];
+      expect(checkWinOrDraw(grid)).toBe(undefined);
+    });
+  });
+  describe("status: win", () => {
+    describe("Rows:", () => {
+      test("returns 'Player 1 Wins' if the sum of grid[0], grid[1], grid[2] is equal to 3", () => {
+        const grid = [
+            1, 1, 1,
+            0, -1, -1,
+            0, 0, 0
+        ];
+        expect(checkWinOrDraw(grid)).toBe('Player 1 Wins');
+      });
+      test("returns 'Player 2 Wins' if the sum of grid[0], grid[1], grid[2] is equal to -3", () => {
+        const grid = [
+            -1, -1, -1,
+            0, 1, 1,
+            0, 0, 0
+        ];
+        expect(checkWinOrDraw(grid)).toBe('Player 2 Wins');
+      });
+      test("returns 'Player 1 Wins' if the sum of grid[3], grid[4], grid[5] is equal to 3", () => {
+        const grid = [
+            -1, -1, 0,
+            1, 1, 1,
+            0, 0, 0
+        ];
+        expect(checkWinOrDraw(grid)).toBe('Player 1 Wins');
+      });
+      test("returns 'Player 2 Wins' if the sum of grid[3], grid[4], grid[5] is equal to -3", () => {
+        const grid = [
+            1, 1, 0,
+            -1, -1, -1,
+            0, 0, 0
+        ];
+        expect(checkWinOrDraw(grid)).toBe('Player 2 Wins');
+      });
+      test("returns 'Player 1 Wins' if the sum of grid[6], grid[7], grid[7] is equal to 3", () => {
+        const grid = [
+            -1, -1, 0,
+            0, 0, 0,
+            1, 1, 1
+        ];
+        expect(checkWinOrDraw(grid)).toBe('Player 1 Wins');
+      });
+      test("returns 'Player 2 Wins' if the sum of grid[6], grid[7], grid[7] is equal to -3", () => {
+        const grid = [
+            1, 1, 0,
+            0, 0, 0,
+            -1, -1, -1
+        ];
+        expect(checkWinOrDraw(grid)).toBe('Player 2 Wins');
+      });
+    });
+    describe("Columns", () => {
+        test("returns 'Player 1 Wins' if the sum of column is equal to 3", () => {
+            const grid = [
+                1, 0, 0,
+                1, 0, 0,
+                1, -1, -1
+            ];
+            const grid1 = [
+                -1, 1, 0,
+                0, 1, 0,
+                0, 1, -1
+            ];
+            const grid2 = [
+                0, 0, 1,
+                0, 0, 1,
+                -1, -1, 1
+            ];
+            expect(checkWinOrDraw(grid)).toBe("Player 1 Wins")
+            expect(checkWinOrDraw(grid1)).toBe("Player 1 Wins")
+            expect(checkWinOrDraw(grid2)).toBe("Player 1 Wins")
+        })
+        test("returns 'Player 2 Wins' if the sum of column is equal to -3", () => {
+            const grid = [
+                -1, 0, 0,
+                -1, 0, 0,
+                -1, 1, 1
+            ];
+            const grid1 = [
+                1, -1, 0,
+                0, -1, 0,
+                0, -1, 1
+            ];
+            const grid2 = [
+                0, 0, -1,
+                0, 0, -1,
+                1, 1, -1
+            ];
+            expect(checkWinOrDraw(grid)).toBe("Player 2 Wins")
+            expect(checkWinOrDraw(grid1)).toBe("Player 2 Wins")
+            expect(checkWinOrDraw(grid2)).toBe("Player 2 Wins")
+        })
+    })
+    describe("Diagonal:", () => {
+        test("returns 'Player 1 wins if sum of the diagonal is equal to 3", () => {
+            const grid = [
+                1, 0, -1, 
+                0, 1, 0, 
+                -1, 0, 1
+            ]
+            const grid1 = [
+                0, 0, 1, 
+                0, 1, -1, 
+                1, 0, -1
+            ]
 
+            expect(checkWinOrDraw(grid)).toBe("Player 1 Wins")
+            expect(checkWinOrDraw(grid1)).toBe("Player 1 Wins")
+        })
+        test("returns 'Player 2 wins if sum of the diagonal is equal to -3", () => {
+            const grid = [
+                1, 0, -1, 
+                0, -1, 1, 
+                -1, 0, 1
+            ]
+            const grid1 = [
+                1, 0, -1, 
+                0, -1, 1, 
+                -1, 0, 1
+            ]
 
-
-
-    } )
-    // describe("Rows:", ()=>{
-    //     test("", () => {
-    //         const grid = []
-    //     })
-
-    // })
-})
+            expect(checkWinOrDraw(grid)).toBe("Player 2 Wins")
+            expect(checkWinOrDraw(grid1)).toBe("Player 2 Wins")
+        })
+    })
+  });
+});
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,2 +1,20 @@
 exports.checkWinOrDraw = (grid) => {
+    const player1win = "Player 1 Wins"
+    const player2win = "Player 2 Wins"
+    //row win condition
+    for(let i = 0; i < 9; i += 3) {
+        if (grid[i] + grid[i+1] + grid[i+2] === 3) {return player1win}
+        if (grid[i] + grid[i+1] + grid[i+2] === -3) {return player2win}
+    }
+    //column win condition
+    for(let i = 0; i < 3; i += 1) {
+        if (grid[i] + grid[i+3] + grid[i+6] === 3) {return player1win}
+        if (grid[i] + grid[i+3] + grid[i+6] === -3) {return player2win}
+    }
+    //diagonal win condition
+    if (grid[0] + grid[4] + grid[8] === 3) { return 'Player 1 Wins'}
+    if (grid[2] + grid[4] + grid[6] === 3) { return 'Player 1 Wins'}
+    if (grid[0] + grid[4] + grid[8] === -3) { return 'Player 2 Wins'}
+    if (grid[2] + grid[4] + grid[6] === -3) { return 'Player 2 Wins'}
+    return 1 + null
 }
