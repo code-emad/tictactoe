514716c280946bae4b1a904ed6f0c650805311dd
Emad <emadul.islam@hotmail.co.uk>
2023-05-16 20:03:04 +0100

    rendered winner text

--- src/Components/ThreeByThree.jsx
+++ src/Components/ThreeByThree.jsx
@@ -25,7 +25,7 @@ const grid = [
 return (
     <div>
         <p>counter:{counter}</p>
- 
+        <p>{checkWinOrDraw(grid)}</p>
     <div className="box-container">
         <Box setBox={setBox1} box={box1} counter={counter} setCounter={setCounter}/>
         <Box setBox={setBox2} box={box2} counter={counter} setCounter={setCounter}/>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,10 +2,14 @@ exports.checkWinOrDraw = (grid) => {
     const player1win = "Player 1 Wins"
     const player2win = "Player 2 Wins"
     //row win condition
+    console.log(true, 0)
     for(let i = 0; i < 9; i += 3) {
+        console.log(true, 2.5)
+        console.log(i, grid[i])
         if (grid[i] + grid[i+1] + grid[i+2] === 3) {return player1win}
         if (grid[i] + grid[i+1] + grid[i+2] === -3) {return player2win}
     }
+    console.log(true, 2)
     //column win condition
     for(let i = 0; i < 3; i += 1) {
         if (grid[i] + grid[i+3] + grid[i+6] === 3) {return player1win}
@@ -16,4 +20,5 @@ exports.checkWinOrDraw = (grid) => {
     if (grid[2] + grid[4] + grid[6] === 3) { return 'Player 1 Wins'}
     if (grid[0] + grid[4] + grid[8] === -3) { return 'Player 2 Wins'}
     if (grid[2] + grid[4] + grid[6] === -3) { return 'Player 2 Wins'}
+    
 }
