7413137b7a33278fdde3b5bdcdbb4ec0e30221d2
Emad <emadul.islam@hotmail.co.uk>
2023-07-08 20:19:54 +0100

    title styled

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import './App.css';
-import { ThreeByThree } from './Components/ThreeByThree';
-import { useContext } from 'react';
-import { PlayerContext } from './contexts/Player'
-
+import "./App.css";
+import { ThreeByThree } from "./Components/ThreeByThree";
+import { useContext } from "react";
+import { PlayerContext } from "./contexts/Player";
 
 function App() {
-  const {player} = useContext(PlayerContext);
+  const { player } = useContext(PlayerContext);
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1 class="">
-          Tic Tac Toe
-        </h1>
-        <p>
-          {player}
-        </p>
+        <h1 className="title text--top">Tic Tac Toe</h1>
+        <p>{player}</p>
+        
       </header>
-      <ThreeByThree/>
+      <ThreeByThree />
     </div>
   );
 }
