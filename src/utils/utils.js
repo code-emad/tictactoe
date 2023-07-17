814ea63f81f0ce9939b6a3d3db13a87a5bfb1698
Emad <emadul.islam@hotmail.co.uk>
2023-07-17 14:48:24 +0100

    changing player 1 or 2 wins to player x or o wins

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,6 @@
 exports.checkWinOrDraw = (grid) => {
-    const player1win = "Player 1 Wins"
-    const player2win = "Player 2 Wins"
+    const player1win = "Player X Wins"
+    const player2win = "Player O Wins"
     //row win condition
     for(let i = 0; i < 9; i += 3) {
         if (grid[i] + grid[i+1] + grid[i+2] === 3) {return player1win}
@@ -12,9 +12,9 @@ exports.checkWinOrDraw = (grid) => {
         if (grid[i] + grid[i+3] + grid[i+6] === -3) {return player2win}
     }
     //diagonal win condition
-    if (grid[0] + grid[4] + grid[8] === 3) { return 'Player 1 Wins'}
-    if (grid[2] + grid[4] + grid[6] === 3) { return 'Player 1 Wins'}
-    if (grid[0] + grid[4] + grid[8] === -3) { return 'Player 2 Wins'}
-    if (grid[2] + grid[4] + grid[6] === -3) { return 'Player 2 Wins'}
+    if (grid[0] + grid[4] + grid[8] === 3) { return 'Player X Wins'}
+    if (grid[2] + grid[4] + grid[6] === 3) { return 'Player X Wins'}
+    if (grid[0] + grid[4] + grid[8] === -3) { return 'Player O Wins'}
+    if (grid[2] + grid[4] + grid[6] === -3) { return 'Player O Wins'}
     
 }
